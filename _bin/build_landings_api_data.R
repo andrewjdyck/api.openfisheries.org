02a0e290f0fdc7de594cfd693fd942d715339eed
andy <andrew@andrewdyck.com>
2018-02-07 18:33:08 -0600

    update codebase for 2017

--- _bin/build_landings_api_data.R
+++ _bin/build_landings_api_data.R
@@ -2,9 +2,9 @@
 source('./_bin/fao_data_utils.r')
 source('./_bin/prepare_api_json_data.r')
 
-data_file <- './data/fishstat_export_s_capture2016.csv'
+data_file <- './data/fishstat_export_s_capture2017.csv'
 in_data <- read_fao_data(data_file)
-clean_fao_data(in_data, years=1950:2014)
+clean_fao_data(in_data, years=1950:2015)
 
 # Creates a tidy dataset
 prep_fao_fishstat_data(out_data)
@@ -24,8 +24,14 @@ dir.create('./api/landings/species/')
 
 output_country_json(country_list)
 output_species_json(species_list)
+output_global_json()
 
 writeLines(
- toJSON(data.frame(version=0.2, url='http://api.openfisheries.org')),
+  toJSON(
+    data.frame(
+      version=0.3, 
+      url='http://api.openfisheries.org'
+    )
+  ),
  './api/api.json'
 )
